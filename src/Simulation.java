222d1f1d5f07687ecaa4e3b88888d7c63ab118a4
Jasmine Quah <jlq17@ic.ac.uk>
2018-03-01 19:20:44 +0000

    Completed simulation code

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Simulation {
 
@@ -11,17 +12,51 @@ public class Simulation {
   List<Vehicle> vehicles;
   int score;
   int numVehicles = 10;
+  IntStream intStream;
+  int bonus;
 
-  public Simulation(int finalTime) {
+  public Simulation(int finalTime, int bonus) {
     time = 0;
     score = 0;
+    this.bonus = bonus;
     this.finalTime = finalTime;
-    vehicles = new ArrayList<>(numVehicles, new Vehicle());
+    //Creates vehicles with numbered id
+    vehicles = new ArrayList<Vehicle>(numVehicles);
+    IntStream.range(0, numVehicles).sequential().forEach(i -> {
+      vehicles.add(i, new Vehicle(0, 0, i));
+    });
   }
 
   public void runSimulation() {
     for (Vehicle v : vehicles) {
       time = 0;
+      while (time < finalTime) { //and still rides left
+        Ride nextRide = v.getNextRide();
+        boolean getBonus = false;
+
+        //Moves to start of next ride
+        time += getTime(v.getX(), v.getY(), nextRide.getStartX(), nextRide.getStartY());
+        v.setPosition(nextRide.getStartX(), nextRide.getStartY());
+
+        //Waits
+        if (time <= nextRide.getStartTime()) {
+          time = getStartTime();
+          getBonus = true;
+        }
+
+        //Drive to finish
+        time += getTime(v.getX(), v.getY(), nextRide.getEndX(), nextRide.getEndY());
+        v.setPosition(nextRide.getEndX(), nextRide.getEndY());
+
+        //If arrives before ride's final time, adds to score
+        if (time <= nextRide.getEndTime) {
+          score += getTime(getRideTime());
+          if (getBonus) {
+            score += bonus;
+          }
+        }
+
+      }
     }
   }
 
