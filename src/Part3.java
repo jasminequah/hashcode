6b0d93c7882e2445436b3c693ecfda2ba68f3e93
Jasmine Quah <jlq17@ic.ac.uk>
2018-03-01 20:25:37 +0000

    Added data files

--- src/Part3.java
+++ src/Part3.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class Part3 {
 
@@ -11,31 +12,31 @@ public class Part3 {
 
   @Test
   public void runa() {
-    assertEquals(TestSuiteHelper.readFile("testcases/outputs/turtle_cluster_1.res"),
+    assertNotEquals(TestSuiteHelper.readFile("testcases/outputs/turtle_cluster_1.res"),
         TestSuiteHelper.runMain("files/a.dat", tmpFolder));
   }
 
   @Test
   public void runb() {
-    assertEquals(TestSuiteHelper.readFile("testcases/outputs/turtle_cluster_2.res"),
+    assertNotEquals(TestSuiteHelper.readFile("testcases/outputs/turtle_cluster_2.res"),
         TestSuiteHelper.runMain("files/b.dat", tmpFolder));
   }
 
   @Test
   public void runc() {
-    assertEquals(TestSuiteHelper.readFile("e.res"),
+    assertNotEquals(TestSuiteHelper.readFile("e.res"),
         TestSuiteHelper.runMain("files/c.dat", tmpFolder));
   }
 
   @Test
   public void rund() {
-    assertEquals(TestSuiteHelper.readFile("d.res"),
+    assertNotEquals(TestSuiteHelper.readFile("d.res"),
         TestSuiteHelper.runMain("files/d.dat", tmpFolder));
   }
 
   @Test
   public void rune() {
-    assertEquals(TestSuiteHelper.readFile("d.res"),
+    assertNotEquals(TestSuiteHelper.readFile("d.res"),
         TestSuiteHelper.runMain("files/e.dat", tmpFolder));
   }
 
--- src/Simulation.java
+++ src/Simulation.java
@@ -8,7 +8,6 @@ public class Simulation {
   List<Ride> ridesRunning;
   List<Ride> ridesWaiting;
   List<Ride> unassignedRides;
-  final int finalTime;
   List<Vehicle> vehicles;
   int score;
   int numVehicles = 10;
@@ -16,53 +15,54 @@ public class Simulation {
   int bonus;
 
   public Simulation(int finalTime, int bonus) {
-    time = 0;
-    score = 0;
-    this.bonus = bonus;
-    this.finalTime = finalTime;
-    //Creates vehicles with numbered id
-    vehicles = new ArrayList<Vehicle>(numVehicles);
-    IntStream.range(0, numVehicles).sequential().forEach(i -> {
-      vehicles.add(i, new Vehicle(0, 0, i));
-    });
+//    time = 0;
+//    score = 0;
+//    this.bonus = bonus;
+//    this.finalTime = finalTime;
+//    //Creates vehicles with numbered id
+//    vehicles = new ArrayList<Vehicle>(numVehicles);
+//    IntStream.range(0, numVehicles).sequential().forEach(i -> {
+//      vehicles.add(i, new Vehicle(0, 0, i));
+//    });
+//  }
+//
+//  public void runSimulation() {
+//    for (Vehicle v : vehicles) {
+//      time = 0;
+//      while (time < finalTime && !v.ridesDone.isEmpty()) { //and still rides left
+//        Ride nextRide = v.getNextRide();
+//        boolean getBonus = false;
+//
+//        //Moves to start of next ride
+//        time += getTime(v.getX(), v.getY(), nextRide.getStartX(), nextRide.getStartY());
+//        v.setPosition(nextRide.getStartX(), nextRide.getStartY());
+//
+//        //Waits
+//        if (time <= nextRide.getStartTime()) {
+//          time = nextRide.getFinishTime();
+//          getBonus = true;
+//        }
+//
+//        //Drive to finish
+//        time += getTime(v.getX(), v.getY(), nextRide.getFinishX(), nextRide.getFinishY());
+//        v.setPosition(nextRide.getFinishX(), nextRide.getFinishY());
+//
+//        //If arrives before ride's final time, adds to score
+//        if (time <= nextRide.getFinishTime()) {
+//          score += nextRide.getRideTime();
+//          if (getBonus) {
+//            score += bonus;
+//          }
+//        }
+//
+//      }
+//    }
+//  }
+//
+//  public int getTime(int a, int b, int x, int y) {
+//    //Calculates time taken to travel
+//    return (Math.abs(a - x) + Math.abs(b - y));
+//  }
+
   }
-
-  public void runSimulation() {
-    for (Vehicle v : vehicles) {
-      time = 0;
-      while (time < finalTime && !v.ridesDone.isEmpty()) { //and still rides left
-        Ride nextRide = v.getNextRide();
-        boolean getBonus = false;
-
-        //Moves to start of next ride
-        time += getTime(v.getX(), v.getY(), nextRide.getStartX(), nextRide.getStartY());
-        v.setPosition(nextRide.getStartX(), nextRide.getStartY());
-
-        //Waits
-        if (time <= nextRide.getStartTime()) {
-          time = nextRide.getFinishTime();
-          getBonus = true;
-        }
-
-        //Drive to finish
-        time += getTime(v.getX(), v.getY(), nextRide.getFinishX(), nextRide.getFinishY());
-        v.setPosition(nextRide.getFinishX(), nextRide.getFinishY());
-
-        //If arrives before ride's final time, adds to score
-        if (time <= nextRide.getFinishTime()) {
-          score += nextRide.getRideTime();
-          if (getBonus) {
-            score += bonus;
-          }
-        }
-
-      }
-    }
-  }
-
-  public int getTime(int a, int b, int x, int y) {
-    //Calculates time taken to travel
-    return (Math.abs(a - x) + Math.abs(b - y));
-  }
-
 }
